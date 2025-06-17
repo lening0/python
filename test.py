5ca718bf417aa46b282d1ad548fb6a07ad0604e4
lening0 <lixiangning88@gmail.com>
2025-06-17 22:16:53 +0800

    6.17
    6.17python练习

---
+++ test.py
@@ -0,0 +1,219 @@
+"""
+注释练习
+"""
+# print("hello world")
+
+# print(333)
+# print(12.14)
+# print("long ge")
+
+
+# money = 50
+# print("钱包还有：",money)
+# print(money)
+#
+#
+# money = money - 10
+# print("花了10还有：",money,"元")
+# money = money - 10
+# print("花了10还有：",money,"元")
+#
+# int_type = type(6565)
+# print(int_type)
+#
+# nub = (866)
+# int_nub = type(nub)
+# print(int_nub)
+
+# num = int("11")
+# float(2)
+# num_str = str(22)
+# print(type(num_str),num_str)
+# print(int,num_str)
+
+# float_num = int(3.2)
+#
+# print(type(float_num),float_num)
+
+# int_num = int(11.9)
+# print(type(int_num),int_num)
+
+# class = 1
+# def = 1
+
+# Class = 1
+# print(Class)
+# WOji95 = 1
+
+# num1 = 5
+# num2 = 2
+# ret = num1 + num2
+# print(ret)
+
+# print("1+1=",1+1)
+# print("1-1=",1-1)
+# print("1*1=",1*1)
+# print("1/1=",1/1)
+# print("11//2=",11//2)
+# print("11%2=",11%2)
+# print("2**3=",2**3)
+# print("2**-1=",2**-1)
+
+# num = 1 + 2 * 3
+# num += 1
+# num -= 1
+# num /= 2
+# num %= 2
+# num **=2
+# num //= 2
+# # nub = 1
+# print(num)
+
+# namu = """wo
+# ni
+# die"""
+# print(namu)
+
+# name = ('niu bi')
+# print(name)
+
+# name = '"hello world"'
+
+# name = "\"hello world\""
+# name = '\'hello world\''
+
+# print(name)
+
+# name = 'hhg'
+#
+# print('你看你ma呢你信不信我'+name+'干死你')
+
+# tel = 515
+# print('niubi'+tel+' le le')
+
+# age = 7
+# num = 6
+#
+# money = 0.1
+# name = 'li'
+# print('我叫%s我的年龄是%d,我遇到过%d个老师,我的存款有%f' % (name,age,num,money))
+
+# num = 11.26
+# AAA=('我有%6.1f' % num)
+# print(AAA)
+
+# num = 11.3659
+# AAA = ('我有%.3f' % num)
+# print(AAA)
+
+# name = 'lixi'
+# age = 20
+# height = 172.3
+# print(f'wo shi {name},wo you {age} sui,wo shen gao {height} ')
+
+# /
+
+# name = 'jnzy'
+# stock_price = 19.99
+# stock_code = '00256'
+# stock_daily_growth = 1.2
+# growth_days = 7
+# finally_price = stock_price * (stock_daily_growth **  growth_days)
+# print(f"""
+# 公司：{name},股票代码：{stock_code},当前股价:{stock_price}""")
+# print('每日增长系数：%.1f,经过%d增长后，股价达到了%.2f' % (stock_daily_growth,growth_days,finally_price)  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
