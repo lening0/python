9d2ed9a04c95219bcfc273f7a842dcddc1b94bf0
lening0 <lixiangning88@gmail.com>
2025-07-07 22:36:38 +0800

    Add files via upload

---
+++ 2025.7.7.py
@@ -0,0 +1,221 @@
+# from pyexpat.errors import messages
+#
+# print('hello world')
+# message = 'hello world'
+# print(message)
+from os import remove
+from tkinter.font import names
+
+# print("hello world'ol'")
+
+# name = "ada lovelace"
+# print(name.title())
+
+# n1 = 'aDd'
+# n2 = 'aDD'
+# print(n1.title(), n2.title())
+
+# print('python')
+#
+# print('\tpython\tli\nning')
+
+# print('Languages:\n\tpython\n\tjava\n\tc')
+
+# n = "'lixiangning '"
+# print(n.rstrip())
+# n1 = 'lixiangning'
+# print(n1)
+
+# n = "'python "
+# print(n)
+# n1 = n.rstrip()
+# print(n1)
+
+# n = 'python '
+# n = n.rstrip()
+# print(n)
+
+# n = ' loa pa '
+# n = n.strip()
+# print(n)
+
+# n = 'eric'
+# print(f'Hello {n.title()},would you like to know?')
+
+# n = ' Zeng Guofan '
+# s = '"人但有恒，事无不成。"'
+# print(f'{n.strip()} once said\n\t {s}')
+
+# n = 3 ** 2
+# n1 = 3 / 2
+# print(n,n1)
+
+# print(0.1 + 0.2)
+
+# print(2 / 1)
+# _a = 1
+# print(_a)
+
+# x,y,z = 1,2,3
+# print(x,y,z)
+
+# import this
+
+# names = ['li','wang','zhang']
+# # print(names[0])
+# # print(names[1])
+# # print(names[2])
+#
+# # print(f'hello,{names[0]}')
+# # print(f'hello,{names[1]}')
+# # print(f'hello,{names[2]}')
+# # print(names)
+# # names[0] = 'liu'
+# # print(names)
+#
+# names.append('liu')
+# print(names)
+#
+# names.insert(1,'chen')
+# print(names)
+#
+# del names[1]
+# print(names)
+#
+# names.remove('li')
+# print(names)
+
+# names = ['li','zheng','mao','zeng']
+#
+# print(f"i want invite {names} to my party")
+# no = 'li'
+# print(f"i want invite {names} to my party,but {no} is cant come")
+#
+# names.remove(no)
+# names.append('chen')
+# print(f'So i invite {names} to my party')
+#
+# print('i only can invite two people to my party')
+# n1 = names.pop(2)
+# n2 = names.pop(2)
+# print(f"sorry to {n1}")
+# print(f"sorry to {n2}")
+#
+# print(f"welcome to my party {names}")
+#
+# del names[0]
+# del names[0]
+# print(names)
+
+
+# cars = ['bmw', 'audi', 'toyota','benz']
+
+# print(cars)
+#
+# cars.sort()
+#
+# print(cars)
+#
+# print(sorted(cars, reverse=True))
+#
+# print(cars)
+
+# cars.reverse()
+# print(cars)
+
+# print(len(cars))
+
+# my_goal = ['china','japan','america','india','australia']
+#
+# print(my_goal)
+#
+# print(sorted(my_goal))
+#
+# print(my_goal)
+#
+# my_goal.reverse()
+# print(my_goal)
+#
+# my_goal.reverse()
+# print(my_goal)
+#
+# my_goal.sort()
+# print(my_goal)
+# my_goal.sort(reverse=True)
+# print(my_goal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
