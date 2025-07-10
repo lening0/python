004e674770727151c579ac515694222a2f0d36e3
lening0 <lixiangning88@gmail.com>
2025-07-10 22:50:23 +0800

    Add files via upload
    2025.7.10

---
+++ 2025.7.10.py
@@ -0,0 +1,251 @@
+# print('hello world')
+# print('hello')
+# cars = ['audi','bmw','subaru','toyota']
+# for car in cars:
+#     if car == 'bmw':
+#         print(car.upper())
+#     else:
+#         print(car)
+
+# n = [1,2,3,4,5]
+# 1 in n
+
+# requested_toppings = ['mushrooms', 'onions', 'pineapple']
+# if 'mushrooms' in requested_toppings:
+#     print('yes')
+
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
+# if user not in banned_users:
+#     print(f"{user.title()}, you can post a response if you wish.")
+
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+
+# age = 19
+# if age >= 18:
+#     print("you are a really man")
+# else:
+#     print("you cant do anything")
+
+
+# car = 'audi'
+# if car.upper() == 'AUDI':
+#     print("yes")
+# else:
+#     print('no')
+
+# alien_color = ['green','yellow' ,'red']
+# if 'green' in alien_color:
+#     print('5')
+# elif 'yellow' in alien_color:
+#     print("10")
+# else:
+#     print('15')
+
+# a = ['li','wang','liu','admin','zheng']
+# if a:
+#     for i in a:
+#         if i == 'admin':
+#             print("Hello admin, would you like to see a status report?")
+#         else:
+#             print(f"Hello {i}, thank you for logging in again.")
+# else:
+#     print("We need to find some users!")
+
+# current_users = ['li','wang','liu','admin','zheng']
+# new_users = ['cao','guan','deng','admin','zheng']
+# for i in current_users:
+#     if i in new_users:
+#         print("please choose other")
+#     else:
+#         print(f"{i} can use")
+
+# n = [1,2,3,4,5,6,7,8,9]
+# for i in n:
+#     if i == 1:
+#         print("1st")
+#     elif i == 2:
+#         print("2nd")
+#     elif i == 3:
+#         print("3rd")
+#     else:
+#         print(f"{i}th")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
